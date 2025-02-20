1b733c1d10a1ae3879e80f1b101cb33c1acb36d1
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-20 16:25:33 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,14 +8,14 @@ CONTAINER_NAME="ccubank-core"
 echo "Pulling the latest image from $REGISTRY/$IMAGE_NAME:$TAG..."
 docker pull $REGISTRY/$IMAGE_NAME:$TAG
 
-echo "Stopping and removing any existing container..."
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+#echo "Stopping and removing any existing container..."
+#docker stop $CONTAINER_NAME || true
+#docker rm $CONTAINER_NAME || true
 
-echo "Starting a new container..."
-docker run -d --name $CONTAINER_NAME -p 8000:8000 $REGISTRY/$IMAGE_NAME:$TAG
+#echo "Starting a new container..."
+#docker run -d --name $CONTAINER_NAME -p 8000:8000 $REGISTRY/$IMAGE_NAME:$TAG
 
-echo "Cleaning up unused Docker images..."
-docker system prune -af
+#echo "Cleaning up unused Docker images..."
+#docker system prune -af
 
 echo "Deployment completed successfully."
