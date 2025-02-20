f65b3b0c015cb5df15d3195409b402caae5665b6
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-20 16:16:07 +0700

    Add ci

---
+++ scripts/setup_ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo "SSH_PRIVATE_KEY" | \
+  sed 's/-----BEGIN RSA PRIVATE KEY-----//g' | \
+  sed 's/-----END RSA PRIVATE KEY-----//g' | \
+  sed 's/ /\n/g' | \
+  sed '1s/^/-----BEGIN RSA PRIVATE KEY-----\n/' > server-private-key.pem
+echo -n "-----END RSA PRIVATE KEY-----" >> server-private-key.pem
+chmod 600 server-private-key.pem
+cp server-private-key.pem ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+rm -f server-private-key.pem
+echo "SSH private key setup completed and stored at ~/.ssh/id_rsa"
