eb5ef04233b9c23b452e817eedacf9658ad418ac
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-20 18:15:32 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "Ensuring the compose directory exists on the server..."
-ssh -o StrictHostKeyChecking=no "$WEB_SERVER_SSH_HOST" "mkdir -p $COMPOSE_FILE_DIR"
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./docker-compose.development $WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME
 
-echo "Copying docker-compose.yaml and necessary files to the server..."
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./docker-compose.development.yaml "$WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME"
-
-echo "Deployment completed successfully. $COMPOSE_FILE_NAME - $WEB_SERVER_SSH_HOST"
-echo "COMPOSE_FILE_DIR: $COMPOSE_FILE_DIR"
-echo "COMPOSE_FILE_NAME: $COMPOSE_FILE_NAME"
-echo "WEB_SERVER_SSH_HOST: $WEB_SERVER_SSH_HOST"
+ssh $WEB_SERVER_SSH_HOST << EOF
+    docker system prune -af
+    docker login sjc.vultrcr.com -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
+    docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME pull
+    docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME up -d
+EOF
+sleep 30
