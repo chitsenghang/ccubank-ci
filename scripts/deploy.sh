000f828314bd3bc3ede26b73465f3d518251edf2
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-20 23:14:39 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./docker-compose.development.yaml root@$WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME
 
-# Ensure the compose file exists locally
-if [[ ! -f ./docker-compose.development.yml ]]; then
-    echo "Error: docker-compose.development.yml does not exist!"
-    exit 1
-fi
-
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./docker-compose.development.yml $WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME
-
-# Execute deployment commands on the remote server
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $WEB_SERVER_SSH_HOST << EOF
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$WEB_SERVER_SSH_HOST << EOF
     export REGISTRY_USERNAME=$REGISTRY_USERNAME
     export REGISTRY_PASSWORD=$REGISTRY_PASSWORD
     docker system prune -af
     echo $REGISTRY_PASSWORD | docker login sjc.vultrcr.com -u $REGISTRY_USERNAME --password-stdin
     docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME pull
     docker compose -f $COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME up -d
-
-    # Debugging: Check running containers
     sleep 5
     docker ps -a
 EOF
--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-echo $SSH_PRIVATE_KEY | sed 's/-----BEGIN RSA PRIVATE KEY-----/''/g; s/ -----END RSA PRIVATE KEY-----/''/g' | sed 's/ /\n/g' | sed '1s/$/'"-----BEGIN RSA PRIVATE KEY-----"'/' > server-private-key.pem
-echo -n "-----END RSA PRIVATE KEY-----" >> server-private-key.pem
-chmod 600 server-private-key.pem
-cp server-private-key.pem ~/.ssh/id_rsa
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+ssh-keyscan -H $WEB_SERVER_SSH_HOST >> ~/.ssh/known_hosts
