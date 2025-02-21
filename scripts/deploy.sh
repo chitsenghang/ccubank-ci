84272495227b54ec6a9019b74cf1f413a7471f68
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 10:32:06 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x  # Enable debug mode
 
-echo "Checking if /root/ccubank exists..."
-ssh root@$WEB_SERVER_SSH_HOST "ls -la /root/ccubank"
+echo "Checking if ./ccubank exists..."
+ssh root@$WEB_SERVER_SSH_HOST "ls -la ./ccubank"
 
 echo "Copying files..."
-scp -o StrictHostKeyChecking=no docker-compose.development.yaml root@$WEB_SERVER_SSH_HOST:/root/ccubank/
+scp -o StrictHostKeyChecking=no docker-compose.development.yaml root@$WEB_SERVER_SSH_HOST:/ccubank/
 
 echo "Verifying copied files..."
-ssh root@$WEB_SERVER_SSH_HOST "ls -la /root/ccubank"
+ssh root@$WEB_SERVER_SSH_HOST "ls -la ./ccubank"
 
 set +x  # Disable debug mode
