872c90a25612b2ede82bd26a3c71e0247695b574
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 10:04:30 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
+set -e  # Exit on error
 
-# Exit on any error
-set -e
+REMOTE_DIR="/root/ccubank"
+COMPOSE_FILE="docker-compose.development.yaml"
 
-# Copy docker-compose file to server
-scp -o StrictHostKeyChecking=no \
-    -r ./docker-compose.development.yaml \
-    root@$WEB_SERVER_SSH_HOST:$COMPOSE_FILE_DIR/$COMPOSE_FILE_NAME
+ssh -o StrictHostKeyChecking=no user@${WEB_SERVER_SSH_HOST} << EOF
+    mkdir -p $REMOTE_DIR
+EOF
 
-# Execute deployment commands on remote server
-ssh -o StrictHostKeyChecking=no root@$WEB_SERVER_SSH_HOST << 'EOF'
-    # Exit on any error
-    set -e
+scp -o StrictHostKeyChecking=no docker-compose.development.yaml root@${WEB_SERVER_SSH_HOST}:$REMOTE_DIR/
 
-    # Export registry credentials
-    export REGISTRY_USERNAME=$REGISTRY_USERNAME
-    export REGISTRY_PASSWORD=$REGISTRY_PASSWORD
-
-    # Clean up unused Docker resources
-    docker system prune -af
-
-    # Login to registry
-    echo $REGISTRY_PASSWORD | docker login sjc.vultrcr.com -u $REGISTRY_USERNAME --password-stdin
-
-    # Pull and deploy new containers
-    cd $COMPOSE_FILE_DIR
-    docker compose -f $COMPOSE_FILE_NAME pull
-    docker compose -f $COMPOSE_FILE_NAME up -d
-
-    # Wait for containers to start and show status
-    sleep 5
-    docker ps -a
-
-    # Logout from registry
-    docker logout sjc.vultrcr.com
+ssh -o StrictHostKeyChecking=no user@${WEB_SERVER_SSH_HOST} << EOF
+    cd $REMOTE_DIR
+    docker-compose -f $COMPOSE_FILE pull
+    docker-compose -f $COMPOSE_FILE up -d --remove-orphans
 EOF
