acffb05cbd692321a9314e50b6ced9e8fb7b0fe5
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 11:33:33 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,12 @@
 set -x  # Enable debug mode
 
 echo "Checking if ./ccubank exists..."
-ssh root@$WEB_SERVER_SSH_HOST "ls -la ./ccubank"
+ssh $WEB_SERVER_SSH_HOST "ls -la ./ccubank"
 
 echo "Copying files..."
-scp -o StrictHostKeyChecking=no docker-compose.development.yaml root@$WEB_SERVER_SSH_HOST:/ccubank/
+scp -o StrictHostKeyChecking=no docker-compose.development.yaml $WEB_SERVER_SSH_HOST:/ccubank/
 
 echo "Verifying copied files..."
-ssh root@$WEB_SERVER_SSH_HOST "ls -la ./ccubank"
+ssh $WEB_SERVER_SSH_HOST "ls -la ./ccubank"
 
-set +x  # Disable debug mode
+set +x
--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-set -x
+set -e
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-echo -e "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
+echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\n" > ~/.ssh/config
+
+echo "$WEB_SERVER_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -H "$WEB_SERVER_SSH_HOST" >> ~/.ssh/known_hosts
-
-ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -vvv root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
-
-set +x
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
