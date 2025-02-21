002d7778e0c9b0608a551ccc6d84ad81197f6040
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 11:08:45 +0700

    Add ci

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-
-set -x
-
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-
-# Ensure SSH key is formatted correctly
-echo -e "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo $SSH_PRIVATE_KEY | sed 's/-----BEGIN RSA PRIVATE KEY-----/''/g; s/ -----END RSA PRIVATE KEY-----/''/g' | sed 's/ /\n/g' | sed '1s/$/'"-----BEGIN RSA PRIVATE KEY-----"'/' > server-private-key.pem
+echo -n "-----END RSA PRIVATE KEY-----" >> server-private-key.pem
+chmod 600 server-private-key.pem
+cp server-private-key.pem ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-
-# Add server to known_hosts
-ssh-keyscan -H "$WEB_SERVER_SSH_HOST" >> ~/.ssh/known_hosts
-
-# Generate public key for verification
-ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-
-# Test SSH connection
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -vvv root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
-
-set +x  # Disable debug mode
