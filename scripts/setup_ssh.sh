712e074693a61955ea30e7920ce28334d4b60a36
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 14:17:55 +0700

    Add ci

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-set -e
-
 mkdir -p ~/.ssh
+
 chmod 700 ~/.ssh
 
-echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\n" > ~/.ssh/config
+ssh-keyscan -H $WEB_SERVER_SSH_HOST >> ~/.ssh/known_hosts
 
 echo "$WEB_SERVER_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
 
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
