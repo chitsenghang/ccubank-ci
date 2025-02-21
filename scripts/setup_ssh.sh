2c470e95c07c52d4f45d0add487edd84be4d199d
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 11:26:52 +0700

    Add ci

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
-set -e
 
-# Create the .ssh directory with appropriate permissions
+set -x
+
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-# Disable StrictHostKeyChecking to prevent issues with the host key
-echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\n" > ~/.ssh/config
-
-# Write the private key into the file and ensure proper permissions
-echo "$SH_PRIVATE_KEY" > ~/.ssh/id_rsa
+echo -e "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# Debugging Step: Check the key file's content
-echo "Private key content:"
-cat ~/.ssh/id_rsa
+ssh-keyscan -H "$WEB_SERVER_SSH_HOST" >> ~/.ssh/known_hosts
 
-# Start the SSH agent
-eval "$(ssh-agent -s)"
-echo "SSH Agent Started: $SSH_AGENT_PID"
+ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
-# Add the private key to the SSH agent
-ssh-add ~/.ssh/id_rsa
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -vvv root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
 
-# Check the status of loaded keys in the agent
-ssh-add -l
-
-# Test SSH connection to the server
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
-
+set +x
