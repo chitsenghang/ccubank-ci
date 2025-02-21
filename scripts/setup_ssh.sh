25ea232c328fe2fa2346826064bb961bae592ac2
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-21 10:58:56 +0700

    Add ci

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
@@ -9,16 +7,8 @@ chmod 700 ~/.ssh
 echo -e "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# Verify key content (only first 5 lines for security)
-head -n 5 ~/.ssh/id_rsa
-
-# Add server to known_hosts
-ssh-keyscan -H "$WEB_SERVER_SSH_HOST" >> ~/.ssh/known_hosts
-
 # Generate public key for verification
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
 # Test SSH connection
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -vvv root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
-
-set +x  # Disable debug mode
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -v root@$WEB_SERVER_SSH_HOST "echo 'SSH connection successful'"
