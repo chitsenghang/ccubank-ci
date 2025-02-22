b07a276b4873431212161038d55577b9be7b2d6d
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-22 20:46:49 +0700

    Add ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
 
-# deploy.sh
-set -e
+set -eu
 
-echo "Starting deployment process..."
+SSH_PORT="${SSH_PORT:-22}"
+COMPOSE_FILE_NAME="${COMPOSE_FILE_NAME:-docker-compose.development.yaml}"
 
-# Create remote directory if it doesn't exist
-ssh -i ~/.ssh/id_rsa "$SERVER_USER@$SERVER_HOST" "mkdir -p $COMPOSE_FILE_DIR"
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 
-# Copy docker-compose file to remote server
-echo "Copying docker-compose file..."
-scp -i ~/.ssh/id_rsa "$COMPOSE_FILE_NAME" \
+echo "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' > ~/.ssh/deploy_key
+chmod 600 ~/.ssh/deploy_key
+
+ssh-keyscan -p "$SSH_PORT" -H "$SERVER_HOST" >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+
+cat > ~/.ssh/config << EOF
+Host deployment_target
+    HostName $SERVER_HOST
+    User $SERVER_USER
+    Port $SSH_PORT
+    IdentityFile ~/.ssh/deploy_key
+    StrictHostKeyChecking accept-new
+    UserKnownHostsFile ~/.ssh/known_hosts
+EOF
+
+chmod 600 ~/.ssh/config
+
+remote_command() {
+    ssh -p "$SSH_PORT" -i ~/.ssh/deploy_key "$SERVER_USER@$SERVER_HOST" "$1"
+}
+
+remote_command "
+    if [ ! -d \"$COMPOSE_FILE_DIR\" ]; then
+        mkdir -p \"$COMPOSE_FILE_DIR\"
+        echo 'Directory created successfully'
+    else
+        echo 'Directory already exists'
+    fi
+    chmod 755 \"$COMPOSE_FILE_DIR\"
+"
+scp -P "$SSH_PORT" -i ~/.ssh/deploy_key \
+    "$COMPOSE_FILE_NAME" \
     "$SERVER_USER@$SERVER_HOST:$COMPOSE_FILE_DIR/"
 
-# Login to container registry
-echo "Logging into container registry..."
-ssh -i ~/.ssh/id_rsa "$SERVER_USER@$SERVER_HOST" \
-    "docker login -u $REGISTRY_USERNAME -p $REGISTRY_TOKEN"
+remote_command "docker login $REGISTRY -u $REGISTRY_USERNAME -p $REGISTRY_TOKEN"
 
-# Pull and deploy using docker-compose
-echo "Deploying application..."
-ssh -i ~/.ssh/id_rsa "$SERVER_USER@$SERVER_HOST" "cd $COMPOSE_FILE_DIR && \
+remote_command "cd $COMPOSE_FILE_DIR && \
     docker-compose -f $COMPOSE_FILE_NAME pull && \
     docker-compose -f $COMPOSE_FILE_NAME up -d"
 
