a06d7f2d218fc28fd0f4f121d02b05c3582e1593
Chit-Senghang <senghang.chit@igsaas.asia>
2025-02-27 09:53:50 +0700

    Test

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     })
   );
   const config = new DocumentBuilder()
-    .setTitle('API ccubank core api documentation')
+    .setTitle('API ccubank core api documentation 123')
     .setDescription('The api ccubank for website')
     .setVersion('1.0')
     .addBearerAuth()
